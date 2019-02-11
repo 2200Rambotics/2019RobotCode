67ab9bb274c4c6e0ace7a8a848f12abbca0cb5f4
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-02-11 00:50:16 -0500

    Add empty classes

--- src/main/include/Drive.h
+++ src/main/include/Drive.h
@@ -3,10 +3,15 @@
 #include <frc/WPILib.h>
 #include <ctre/Phoenix.h>
 
+#include "Constants.h"
+
 class Drive {
   public:
   Drive();
 
   private:
-
+  WPI_TalonSRX leftMotor1{Constants::leftDriveMotorID1};
+  WPI_TalonSRX leftMotor2{Constants::leftDriveMotorID2};
+  WPI_TalonSRX rightMotor1{Constants::rightDriveMotorID1};
+  WPI_TalonSRX rightMotor2{Constants::rightDriveMotorID2};
 };
