27a5d7e5e6bf2f4cbed98f3e18e0656362081914
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-02-11 00:18:00 -0500

    Add Joystick debouncing class

---
+++ src/main/include/JoyStickDebounced.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <frc/Joystick.h>
+
+class JoyStickDebounced : public frc::Joystick {
+  public:
+
+  bool getButtonDebounced(int num) {
+    return buttons[num] && !last_buttons[num];
+  }
+
+  bool getButton(int num) {
+    return buttons[num];
+  }
+
+  void updateButtons() {
+    for (int i = 0; i < 11; ++i) {
+      last_buttons[i] = buttons[i];
+      buttons[i] = this->GetRawButton[i];
+    }
+  }
+
+  private:
+  bool buttons[11];
+  bool last_buttons[11];
+};
